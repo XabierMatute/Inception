1293747bd1ce374ee568f48f8c1a0bc5c0124218
Xabier Matute Rusinol <xmatute-@c3r2s2.42urduliz.com>
2024-04-20 19:00:44 +0200

    lo de /tmp/init.sql

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -3,6 +3,14 @@
 # start mysql
 service mysql start
 
+#create init.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;
+FLUSH PRIVILEGES ;
+" > /tmp/init.sql
+
 # create database and user
 mysql -u root < /tmp/init.sql
 
