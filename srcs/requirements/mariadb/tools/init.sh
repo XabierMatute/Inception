4a3e181a023e35f003891fa2fd019c85f991e40e
Xabier Matute Rusinol <xmatute-@c3r4s2.42urduliz.com>
2024-04-22 14:17:01 +0200

    deberia estar mejor ahora y mas controlado

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+# Stop the script if any command fails
+set -e
+
+echo "checking variables"
+# Verificar que las variables de entorno estén establecidas
+if [ -z "$MYSQL_DATABASE" ]; then
+    echo "Error: MYSQL_DATABASE is not set"
+    exit 1
+fi
+
+if [ -z "$MYSQL_USER" ]; then
+    echo "Error: MYSQL_USER is not set"
+    exit 1
+fi
+
+if [ -z "$MYSQL_PASSWORD" ]; then
+    echo "Error: MYSQL_PASSWORD is not set"
+    exit 1
+fi
+
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+    echo "Error: MYSQL_ROOT_PASSWORD is not set"
+    exit 1
+fi
+echo "variables checked!"
+
+
 # start mysql in the background
 mysqld_safe &
 
@@ -9,9 +36,9 @@ CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;
 FLUSH PRIVILEGES ;
-" > /tmp/init.sql
+" > /tmp/XMinit.sql
 
-echo "init.sql created"
+echo "XMinit.sql created"
 
 # wait for mysql to start
 while ! mysqladmin ping -h localhost ; do
@@ -21,15 +48,15 @@ done
 echo "mysql started"
 
 # create database and user
-mysql -u root < /tmp/init.sql
+mysql -u root < /tmp/XMinit.sql
 
 echo "Database: $MYSQL_DATABASE created"
 
 # remove the file
-rm /tmp/init.sql
+rm /tmp/XMinit.sql
 
 # stop mysql
-mysql -u root -e "SHUTDOWN"
+mysql -u root -p $MYSQL_ROOT_PASSWORD -e "SHUTDOWN"
 
 # start mysql in the background
 mysqld --bind-address=0.0.0.0
